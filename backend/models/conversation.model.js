44a354d908bc11ef6f3da2456b05c52f2e67583c
HenriqueOliveira29 <henrique.olive29@gmail.com>
2024-03-16 17:12:31 +0000

    Create backend and some implementations

---
+++ backend/models/conversation.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const ConversationSchema = new mongoose.Schema({
+    participants:[ 
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+        }
+    ],
+    messages: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Message',
+            default: []
+        }
+    ],
+    //createdAt, UpdatedAt
+}, {timestamps: true});
+
+const Conversation = mongoose.model("Conversation", ConversationSchema);
+
+export default Conversation;
