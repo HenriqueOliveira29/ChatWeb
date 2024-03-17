280e411f16bf0bcead3e64901644f45c264591b8
HenriqueOliveira29 <henrique.olive29@gmail.com>
2024-03-17 15:56:42 +0000

    get all users in the database

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
 import conversationRoutes from "./routes/conversation.routes.js"
+import userRoutes from "./routes/user.routes.js"
 import connectToMongoDB from "./db/connectToMongoDB.js";
-import cookieParser from "cookie-parser";
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -17,6 +20,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/conversation", conversationRoutes)
+app.use("/api/users", userRoutes)
 
 app.listen(PORT, () => {
     connectToMongoDB();
