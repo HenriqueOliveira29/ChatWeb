9e340a74fe16b9594627d2f53dd71d4805cbc602
HenriqueOliveira29 <henrique.olive29@gmail.com>
2024-03-18 12:16:08 +0000

    create home page

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,9 +1,15 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import Sidebar from '../../components/sidebar/Sidebar.jsx'
+import MessageContainer from '../../components/messages/MessageContainer.jsx'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='flex sm:h-[450px] md:h-[550] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+      <Sidebar/>
+      <MessageContainer/>
+
+    </div>
   )
 }
 
